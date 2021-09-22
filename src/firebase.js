e2b7e4c3e364378d95146d133ead9e48b6ab737e
seondal <sseondal@gmail.com>
2021-09-22 19:47:06 +0900

    3장 트위터 클론 코딩 시작하기
    03.1 파이어베이스의 비밀키 숨기기
    03.2 라우터 준비하기
    03.3 라우터 적용하기

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkf-10PLNDXWC9OIdp1k2n-PmwBMbFcq8",
-  authDomain: "clone-twitter-4d5ce.firebaseapp.com",
-  projectId: "clone-twitter-4d5ce",
-  storageBucket: "clone-twitter-4d5ce.appspot.com",
-  messagingSenderId: "221231866268",
-  appId: "1:221231866268:web:8b75336fbe51264a706859",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
