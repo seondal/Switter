4a1b468940d89e6cefafd2ad1a63dd394d7426ab
seondal <sseondal@gmail.com>
2021-09-22 14:21:16 +0900

    1.0 initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkf-10PLNDXWC9OIdp1k2n-PmwBMbFcq8",
+  authDomain: "clone-twitter-4d5ce.firebaseapp.com",
+  projectId: "clone-twitter-4d5ce",
+  storageBucket: "clone-twitter-4d5ce.appspot.com",
+  messagingSenderId: "221231866268",
+  appId: "1:221231866268:web:8b75336fbe51264a706859",
+};
+
+const app = initializeApp(firebaseConfig);
