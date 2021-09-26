d9407b338c7cb0cf5ee3f91df5c321a262d5df5e
seondal <sseondal@gmail.com>
2021-09-26 22:08:25 +0900

    07.2 프로필 페이지 보완
    - 내비게이션에 이름넣기
    - 프로필 업데이트 기능 추가
    ((userObj 문서 참고))

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,7 +7,7 @@ import Navigation from "./Navigation";
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation />}
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
           <>
@@ -15,7 +15,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile userObj={userObj} />
             </Route>
           </>
         ) : (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,42 @@
 import { authService } from "fbase";
+import { useState } from "react";
 import { useHistory } from "react-router";
+import { updateProfile } from "@firebase/auth";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
 
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (userObj.displayName != newDisplayName) {
+      updateProfile(userObj, { displayName: newDisplayName });
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
