a14de23a86490cce669db59c2084536891b38bda
seondal <sseondal@gmail.com>
2021-09-26 21:47:28 +0900

    05.4 수정기능
    - 수정기능을 위한 useState 추가
    - 입력란 onChange 작업
    - 파이어스토어에 새 입력값 반영

--- src/components/Sweet.js
+++ src/components/Sweet.js
@@ -1,24 +1,54 @@
 import { dbService } from "fbase";
-import { doc, deleteDoc } from "@firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "@firebase/firestore";
+import { useState } from "react";
 
 const Sweet = ({ sweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newSweet, setNewSweet] = useState(sweetObj.text);
+
   const onDeleteClick = () => {
     const ok = window.confirm("삭제할거임?");
     console.log(ok);
     if (ok) {
       console.log(sweetObj.id);
       deleteDoc(doc(dbService, "sweets", sweetObj.id));
-      //   console.log(data);
     }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewSweet(value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    updateDoc(doc(dbService, "sweets", sweetObj.id), { text: newSweet });
+    setEditing(false);
   };
 
   return (
     <div>
-      <h4>{sweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button onClick={onDeleteClick}>Delete Sweet</button>
-          <button>Edit Sweet</button>
+          <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={newSweet} required />
+            <input type="submit" value="Update Sweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{sweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Sweet</button>
+              <button onClick={toggleEditing}>Edit Sweet</button>
+            </>
+          )}
         </>
       )}
     </div>
